13256f5ec63566688b0c8a829cd73f78bdc629cd
At0m <automaticat0m.conf@gmail.com>
2025-03-21 10:35:10 +0545

    Add files via upload

--- watermelon.cpp
+++ watermelon.cpp
@@ -1,1 +1,20 @@
+/* Watermelon */
 
+#include <iostream>
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int w;
+    cin >> w;
+    if(w%2==0&&w>2){
+        cout << "YES";
+    }
+    else{
+        cout << "NO";
+    }
+ 
+    return 0;
+}
